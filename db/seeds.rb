eb61c2a17f2a5071d7071298e9979f4479afea0b
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-14 23:59:25 +0100

    Create 5 instances of the message model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create!(greetings: 'Good Morning')
+Message.create!(greetings: 'Good Afternoon')
+Message.create!(greetings: 'Good Evening')
+Message.create!(greetings: 'Ndewo')
+Message.create!(greetings: 'Inakwana')
