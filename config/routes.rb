42167ff9c71c82aa7e5eda0c63eb828197a1c9c7
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-15 09:25:31 +0100

    Create root route and route that leads to the App React component

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     get '/greetings', to: 'greetings#index'
   end
+
+  get '*page', to: "static#index", constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root "static#index"
 end
