83ebf04f8a95c0c0aa3c9572b9b2c938035683db
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-15 13:20:40 +0100

    Make the redux store available to all React components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import PropTypes from "prop-types"
+import store from '../redux/configureStore';
 
-import Greeting from './greeting'
+import Greeting from './Greeting'
 
 const App = () => (
   <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<div>Hello Nigeria</div>} />
-        <Route path="/greeting" element={<Greeting />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Hello Nigeria</div>} />
+          <Route path="/greeting" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </Provider>
   </>
 )
-
-// ReactDOM.render(
-//   // <Provider store={store}>
-//     <App />,
-//   // </Provider>,
-//   document.getElementById('root'),
-// );
 
 export default App
